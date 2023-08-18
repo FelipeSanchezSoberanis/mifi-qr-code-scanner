a20b71fb5002da65a3df0a01aa01b7590016510f
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-08-18 16:56:18 -0600

    Force to allow camera use

--- App.tsx
+++ App.tsx
@@ -4,9 +4,31 @@ import { PaperProvider } from "react-native-paper";
 import { StudentRegistration } from "./src/types";
 import StudentRegistrationsListComponent from "./src/components/student-registration-list-component";
 import ButtonGroupComponent from "./src/components/button-group-component";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Text } from "react-native-paper";
 
 export default function Main() {
   const [studentRegistrations, setStudentRegistrations] = React.useState<StudentRegistration[]>([]);
+  const [hasCameraPermission, setHasCameraPermission] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    async function getCameraPermission() {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasCameraPermission(status === "granted");
+    }
+
+    getCameraPermission();
+  }, []);
+
+  if (!hasCameraPermission) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text variant="titleLarge" style={{ textAlign: "center" }}>
+          La cámara es necesaria para poder utilizar la aplicación
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <PaperProvider>
